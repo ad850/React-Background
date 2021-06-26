9e33b652249bd16be737ebe1ac90597a147ba3b6
aditya <aditya.sharma9827.as@gmail.com>
2021-06-27 00:15:48 +0530

    Updates

---
+++ precache-manifest.8e1498e5f4e5159eef86fb06c6d0e8d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c65e00c946b6ab6c102",
+    "url": "/React-Background/static/css/main.6460475f.chunk.css"
+  },
+  {
+    "revision": "2c65e00c946b6ab6c102",
+    "url": "/React-Background/static/js/main.50809d74.chunk.js"
+  },
+  {
+    "revision": "def498284fca2c73e5f2",
+    "url": "/React-Background/static/js/runtime~main.3ff72e01.js"
+  },
+  {
+    "revision": "a65c171dbbbcbbee4149",
+    "url": "/React-Background/static/js/2.becd5d47.chunk.js"
+  },
+  {
+    "revision": "c45b28fee88977ebbbcfc3017b6819b4",
+    "url": "/React-Background/static/media/image.c45b28fe.jpg"
+  },
+  {
+    "revision": "07e6ef3c620faff20fbc4c95605a9005",
+    "url": "/React-Background/index.html"
+  }
+];
